927971a430709d47d0b236cfc415fea48c44a71a
furkanhb <fhbardak@gmail.com>
2022-04-18 00:18:23 +0300

    fuzz

--- hello/hello.go
+++ hello/hello.go
@@ -1,21 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"log"
+// import (
+// 	"fmt"
+// 	"log"
 
-	"example.com/greatings"
-)
+// 	"example.com/greatings"
+// )
 
 func main() {
-	log.SetPrefix("Greatings: ")
-	log.SetFlags(5)
+	// log.SetPrefix("Greatings: ")
+	// log.SetFlags(5)
 
-	names := []string{"Furkan", "Abuzer", "Kadayif"}
+	// names := []string{"Furkan", "Abuzer", "Kadayif"}
 
-	messages, err := greatings.Hellos(names)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(messages)
+	// messages, err := greatings.Hellos(names)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(messages)
 }
